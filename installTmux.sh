8186b42f6b079f0b41e65b5025b4a93c3769ea69
lmm915815 <tanliqing2010@163.com>
2018-03-19 10:39:29 +0800

    5

--- installTmux.sh
+++ installTmux.sh
@@ -3,16 +3,16 @@
 
 which yum  > /dev/null 2>&1  
 if test $?;then
-
     yum install -y git tmux 
-fi
-
-which apt > /dev/null 2>&1
-if test $? -eq 0 ;then
-    echo
-    apt inatall -y git tmux 
+else 
+    which apt > /dev/null 2>&1
+    if test $? -eq 0 ;then
+        apt inatall -y git tmux
+    else
+        echo "Can't ensure os release,please install manual"
+        exit 1
+    fi 
 fi 
-
 
 git clone https://github.com/lmm915815/myTmux.git
 
